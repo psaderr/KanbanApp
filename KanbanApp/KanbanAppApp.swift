5e402105f3fde797aca196d0495e2958f63284ea
Alif <alif_oliveira@hotmail.com>
2025-04-14 17:57:50 -0400

    Initial Commit

---
+++ KanbanApp/KanbanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanAppApp.swift
+//  KanbanApp
+//
+//  Created by Alif on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct KanbanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanbanAppTests/KanbanAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanAppTests.swift
+//  KanbanAppTests
+//
+//  Created by Alif on 14/04/25.
+//
+
+import Testing
+@testable import KanbanApp
+
+struct KanbanAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
